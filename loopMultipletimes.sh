78f82a471771d1cc730a2e093fe510a5540c3a2e
“AHEMMED-1525” <ahemmedhussain1525@gmail.com>
2020-09-02 16:02:56 +0530

    possible outcomes of Heads & Tails

--- loopMultipletimes.sh
+++ loopMultipletimes.sh
@@ -2,14 +2,10 @@
 flip=1
 heads=0
 tails=0
-#function flipCoin() {
-#	flipCheck=$(( RANDOM%2 ))
-#	echo $flipCheck
-#}
+
 while [ $flip -le 15 ]
 do
 	result=$((RANDOM%2))
-	#result="$(flipCoin)"
 	case $result in
 		1)
 		  echo "Heads"$heads
